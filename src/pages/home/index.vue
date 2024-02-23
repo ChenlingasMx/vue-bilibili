2ba302e0065abea6cc575a239d2d383a48126148
ChenlingasMx <42308734@qq.com>
2024-02-24 00:34:25 +0800

    feat: 首页播放动画

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Layout>
-      <a-row :gutter="16" style="margin-top: 20px">
+      <!-- <a-row :gutter="16" style="margin-top: 20px">
         <a-col :span="12">
           <a-carousel autoplay>
             <div v-for="(item, i) in images" :key="i">
@@ -16,11 +16,14 @@
             </div>
           </a-carousel>
         </a-col>
-      </a-row>
+      </a-row> -->
 
       <a-row :gutter="16">
         <a-col :span="6" v-for="item in videos" :key="item.key" style="margin-top: 16px">
           <a-card hoverable>
+            <template #actions>
+              <PlayCircleOutlined @click="watchLater" />
+            </template>
             <template #cover>
               <img :src="item.image" />
             </template>
@@ -29,6 +32,12 @@
         </a-col>
       </a-row>
       <a-spin :spinning="loading" v-show="loading" style="display: flex; justify-content: center" />
+
+      <div class="animate-ball">
+        <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+          <div class="animate" v-show="animate.show"></div>
+        </transition>
+      </div>
     </Layout>
   </div>
 </template>
@@ -37,12 +46,20 @@
 import Layout from '../layouts/default.vue'
 import png0 from '@/assets/0.png'
 import png1 from '@/assets/1.png'
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, reactive } from 'vue'
 import { getIndexList, type Video } from '@/api/mock'
+import { PlayCircleOutlined } from '@ant-design/icons-vue'
 const images = ref([png0, png1])
 let current = 0
 let videos = ref<Video[]>([])
 let loading = ref(false)
+let animate: {
+  show: boolean
+  el: Element | null
+} = reactive({
+  show: false,
+  el: null
+})
 onMounted(async () => {
   const res: Video[] = await getIndexList(current)
   videos.value = res
@@ -69,6 +86,32 @@ const scroll = async () => {
     videos.value = [...videos.value, ...res]
   }
 }
+
+const watchLater = (e: Event) => {
+  // 我们需要一个小球来到鼠标的位置 自动动画移动到稍后再看的位置
+  animate.show = true
+  animate.el = e.target as HTMLElement
+  e.preventDefault()
+}
+
+const beforeEnter = (el: Element) => {
+  let dom = animate.el
+  let rect: any = dom?.getBoundingClientRect()
+  let x = window.innerWidth - rect?.left - 230
+  let y = rect?.top - 10
+  ;(el as HTMLElement).style.transform = `translate(-${x}px,${y}px)`
+}
+
+const enter = (el: Element, done: () => void) => {
+  document.body.offsetHeight
+  ;(el as HTMLElement).style.transform = `translate(0,0)`
+  el.addEventListener('transitionend', done)
+}
+
+const afterEnter = (el: Element) => {
+  ;(el as HTMLElement).style.display = `none`
+  animate.show = false
+}
 </script>
 <style lang="scss">
 .car-imgs {
@@ -79,5 +122,16 @@ const scroll = async () => {
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+}
+.animate-ball .animate {
+  position: fixed;
+  top: 15px;
+  right: 230px;
+  z-index: 100;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #fb7299;
+  transition: all 1s linear;
 }
 </style>
